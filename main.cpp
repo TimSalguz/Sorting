4e534deb17e211d5af967120467625d2b119056c
TimSalguz <timsalguz@gmail.com>
2022-02-05 08:53:47 +0200

    Сделано

--- main.cpp
+++ main.cpp
@@ -13,21 +13,16 @@ int main()
 {
 	const long minrandom = 0;
 	const long maxrandom = 100;
-	long count = 10;
-
-
+	long count = 1000000;
 	std::ofstream befout("input.txt");
 	std::ifstream fin("input.txt");
     std::ofstream fout("output.txt");
-
 	int *array = new int[count];
-
 	//FILL ARRAY
 	for (int i = 0; i < count; ++i)
 	{
 		array[i] = ts::Random(i, minrandom, maxrandom);
 	}
-
 	std::cout << "BEFORE SORT: " << std::endl;
 	for (int i = 0; i < count; ++i)
 	{
@@ -36,32 +31,35 @@ int main()
 
 
 	ts::Timer timer;
-
+	int chet = 0;
+	for (int i = 0; i < count; ++i)
+	{
+		if((array[i] % 2) == 0)
+			chet++;
+	}
+	int *tempArray = new int[count+chet];
+	int j = 0;
 	//SORTING
 	for (int i = 0; i < count; ++i)
 	{
-		if(array[i] % 2 == 0)
+		if ((array[i]%2) != 0)
 		{
-			//Делаю массив больше на 1
-			int *tempArray = new int[count+1];
-			for (int j = 0; j < count; ++j)
-			{
-				tempArray[j] = array[j];
-			}
-			delete[] array;
-			count++;
-
-			//Дублирую элемент, перенося все вправо
-			for(int k = count-1; k > 1; k--) //каунт = 11, поэтому беру 10й элемент, потом беру 9й
-			{
-				array[k] = array[k-1]; //10й элемент равен 9му, потом 9 элемент равен 8му
-			}
-			std::cout << count;
-			i++;
+			tempArray[j] = array[i];
+			j++;
+		}
+		else
+		{
+			tempArray[j] = array[i];
+			j++;
+			tempArray[j] = array[i];
+			j++;
 		}
 	}
-
+	array = tempArray;
+	count += chet;
     std::cout << "Time elapsed: " << timer.elapsed() << '\n';
+
+
 
 	std::cout << "AFTER SORT: " << std::endl;
 	for (int i = 0; i < count; ++i)
